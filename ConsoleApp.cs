608f9984d4be31a8dc6f3fe3259e3907e2b57182
Dilshoda <dilshoda0124@gmail.com>
2024-11-22 15:59:36 +0500

    I created ConsoleApp.cs file

---
+++ ConsoleApp.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
